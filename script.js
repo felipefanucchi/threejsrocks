b27fbe1ca73284537bad63f47f26b59b66ca6a6c
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2020-01-18 16:30:43 -0300

    Adicioando uma esfera com cor amarela e uma luz branca o q da efeito 3d

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setClearColor("#e5e5e5");
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -10,7 +11,17 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
 
-    camera.updateProjectMatrix();
+    camera.updateProjectionMatrix();
 });
+
+const geometry = new THREE.SphereGeometry(1, 10, 10);
+const material = new THREE.MeshLambertMaterial({color:0xFFCC00});
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const light = new THREE.PointLight(0xFFFFFF, 1, 500);
+light.position.set(10, 0, 25);
+scene.add(light);
 
 renderer.render(scene, camera);
